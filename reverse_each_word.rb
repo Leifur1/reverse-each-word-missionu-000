e344e57002343004636dd15658d5fb677792109b
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 20:18:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(sentence)
-  sentence.split(" ")
-  sentence.collect do |i|
-    sentence.reverse
+def reverse_each_word(sentence1)
+  sentence1 = sentence1.split(" ")
+  sentence1.collect do |i|
+    sentence1.reverse
   end
 end
